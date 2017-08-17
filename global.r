18a0ab89ebd09a7284228ee2c24de2efa798676a
ShengweiHuang <huang6275@gmail.com>
2017-08-17 11:22:53 +0800

    v1.21

--- global.r
+++ global.r
@@ -5,18 +5,3 @@ library(RCurl)
 
 currencyType = c("USD", "CAD", "JPY", "HKD", "GBP", "AUD", "SGD", "CHF")
 type1 = c("1 Day", "3 Month", "6 Month")
-
-getData <- function(type1, cType){
-  if (type1 == "1 Day") {
-    url = "http://rate.bot.com.tw/xrt/quote/day/"
-  }
-  else if (type1 == "3 Month") {
-    url = "http://rate.bot.com.tw/xrt/quote/ltm/"
-  }
-  else {
-    url = "http://rate.bot.com.tw/xrt/quote/l6m/"
-  }
-  url = paste(url, cType, sep = "")
-  d = as.data.frame(readHTMLTable(url, header = TRUE, stringsAsFactors = FALSE))
-  return (d)
-}
--- server.r
+++ server.r
@@ -1,4 +1,5 @@
 shinyServer(function(input, output) {
+  # display max and min
   output$outputType <- renderText({
     d = getData(input$type1, input$currencyType)
     strOutput = paste(input$currencyType,
@@ -7,6 +8,7 @@ shinyServer(function(input, output) {
                       "Min =",
                       d[which.min(d[,3]),1], sep=" ")
   })
+  # display plot
   output$mainPlot <- renderPlotly({
     d = getData(input$type1, input$currencyType)
     colnames(d) = c("date", "type", "CashBuy", "CashSell", "SpotBuy", "SpotSell")
@@ -15,9 +17,26 @@ shinyServer(function(input, output) {
       add_trace(y = ~SpotBuy, name = 'Spot Buy') %>%
       add_trace(y = ~SpotSell, name = 'Spot Sell')
   })
+  # display table
   output$mainTable <- renderTable({
     d = getData(input$type1, input$currencyType)
     colnames(d) = c("date", "type", "CashBuy", "CashSell", "SpotBuy", "SpotSell")
     return(d)
   })
 })
+
+# get data from bank of Taiwan
+getData <- function(type1, cType){
+  if (type1 == "1 Day") {
+    url = "http://rate.bot.com.tw/xrt/quote/day/"
+  }
+  else if (type1 == "3 Month") {
+    url = "http://rate.bot.com.tw/xrt/quote/ltm/"
+  }
+  else {
+    url = "http://rate.bot.com.tw/xrt/quote/l6m/"
+  }
+  url = paste(url, cType, sep = "")
+  d = as.data.frame(readHTMLTable(url, header = TRUE, stringsAsFactors = FALSE))
+  return (d)
+}
